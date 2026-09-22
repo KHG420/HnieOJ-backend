975652292391dbbb750447183f04e653482fd6c3
xiexian <>
2026-09-22 12:57:24 +0800

    fix(problem): use canonical sample JSON keys

--- hnieoj-problem/src/main/java/com/hnieacm/problem/dto/ProblemExampleRequest.java
+++ hnieoj-problem/src/main/java/com/hnieacm/problem/dto/ProblemExampleRequest.java
@@ -1,22 +1,19 @@
 package com.hnieacm.problem.dto;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 /**
  * @Author: HaoRan_Lyu
  * @Date: 2026/02/21
- * @Description: 题目样例请求项。存量数据中样例键名为 in/out，通过 JsonAlias 兼容。
+ * @Description: 题目样例请求项，输入/输出字段名为 input/output。
  */
 @Data
 public class ProblemExampleRequest {
 
     @NotBlank(message = "example.input不能为空")
-    @JsonAlias("in")
     private String input;
 
     @NotBlank(message = "example.output不能为空")
-    @JsonAlias("out")
     private String output;
 }
